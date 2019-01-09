8d04f2bc4c4e524128ef1f40e526d3c975002a8d
johnhaus <john.haus@outlook.com>
2019-01-09 18:59:00 -0300

    getting js with brackets (and .)

---
+++ javascript03-object-with-brackets.js
@@ -0,0 +1,27 @@
+// getting a js object with bracket notation
+// - for if object property has space in it's name
+
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+
+var entreeValue = testObj["an entree"];
+var drinkValue = testObj["the drink"];
+
+
+
+
+// and using dot notation...
+
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
